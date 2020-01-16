7da0512701dfb790689e7b5ab9fa5d6dd34560f8
papaousmane21 <papaousmanediene98@gmail.com>
2020-01-16 22:35:49 +0100

    creer_liste_chapitre

--- manga_downloader.py
+++ manga_downloader.py
@@ -2,14 +2,41 @@ import sys
 import os
 from urllib.request import Request, urlopen
 
-def is_exist_chapter(r):
+
+
+def creer_liste_chapitre(chapitre_param):
+
+    if '-' in chapitre_param:
+        list_chapitre = chapitre_param.split('-', maxsplit=1)
+        if is_valid_chapter_syntax(list_chapitre):
+            return range(int(list_chapitre[0]),int(list_chapitre[1]))
+        else:
+            print("Use a valid syntax for chapter list")
+            sys.exit()
+    elif ',' in chapitre_param:
+        list_chapitre = chapitre_param.split(',')
+        if is_valid_chapter_syntax(list_chapitre):
+            return list_chapitre
+        else:
+            print("Use a valid syntax for chapter list")
+            sys.exit()
+    else:
+        if is_valid_chapter_syntax(chapitre_param):
+            return [chapitre_param]
+        else:
+            print("Use a valid syntax for chapter list")
+            sys.exit()
+
+
+
+def check_chapitre_exist(chapitre_url):
     o = 'notexist'
     try:
-        r.index(" is marked as completed. Possibly there will be no new chapter for this manga.")
+        chapitre_url.index(" is marked as completed. Possibly there will be no new chapter for this manga.")
     except ValueError:
         o = 'exist'
 
-    if r == "b''" and o == 'notexist':
+    if chapitre_url == "b''" and o == 'notexist':
         return False
     else:
         return True
